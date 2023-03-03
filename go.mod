67600ea6bf007890a48664a4cfe8de2963d0b9bc
ivarprudnikov <ivar.prudnikov@gmail.com>
2023-03-03 21:18:47 +0000

    render did doc and add issuer to the signature headers

--- go.mod
+++ go.mod
@@ -5,6 +5,19 @@ go 1.20
 require github.com/veraison/go-cose v1.0.0
 
 require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.0-20210816181553-5444fa50b93d // indirect
+	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.0 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.1 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
+)
+
+require (
 	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
+	github.com/lestrrat-go/jwx v1.2.25
 	github.com/x448/float16 v0.8.4 // indirect
 )
--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"crypto/rand"
 	"embed"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/fs"
@@ -14,6 +15,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/veraison/go-cose"
 )
 
@@ -23,6 +25,11 @@ var t time.Time
 
 //go:embed static/*
 var static embed.FS
+
+const KEY_ID = "foobar"
+
+// https://datatracker.ietf.org/doc/draft-ietf-scitt-architecture/
+const ISSUER_HEADER_KEY = 391
 
 func init() {
 	t = time.Now()
@@ -37,12 +44,43 @@ func init() {
 	}
 }
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "ok")
-}
-
 func didHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "did")
+
+	hostport := getDidHostPort()
+
+	key, err := jwk.New(privateKey)
+	if err != nil {
+		sendError(w, "failed to create symmetric key", err)
+		return
+	}
+	if _, ok := key.(jwk.ECDSAPrivateKey); !ok {
+		sendError(w, fmt.Sprintf("expected jwk.SymmetricKey, got %T\n", key), err)
+		return
+	}
+
+	key.Set(jwk.KeyIDKey, KEY_ID)
+	buf, err := json.MarshalIndent(key, "", "  ")
+	if err != nil {
+		sendError(w, "failed to marshal key into JSON", err)
+		return
+	}
+
+	didDoc := fmt.Sprintf(`{
+		"@context": [
+			"https://www.w3.org/ns/did/v1",
+			"https://w3id.org/security/suites/jws-2020/v1"
+		],
+		"id": "did:web:%s",
+		"assertionMethod": [{
+			"id": "did:web:%s#%s",
+			"type": "JsonWebKey2020",
+			"controller": "did:web:%s",
+			"publicKeyJwk": %s
+		}]
+	}`, hostport, hostport, KEY_ID, hostport, buf)
+
+	w.Header().Add("Content-Type", "application/json")
+	fmt.Fprint(w, didDoc)
 }
 
 func signHandler(w http.ResponseWriter, r *http.Request) {
@@ -52,8 +90,7 @@ func signHandler(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
 		body, err = io.ReadAll(r.Body)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, `{ "message": "reading request body failed", "error": "%v" }`, err)
+			sendError(w, "reading request body failed", err)
 			return
 		}
 	}
@@ -63,13 +100,14 @@ func signHandler(w http.ResponseWriter, r *http.Request) {
 	headers := cose.Headers{
 		Protected: cose.ProtectedHeader{
 			cose.HeaderLabelAlgorithm: cose.AlgorithmES256,
+			cose.HeaderLabelKeyID:     []byte(KEY_ID),
+			ISSUER_HEADER_KEY:         []byte("did:web:" + getDidHostPort()),
 		},
 	}
 	// sign and marshal message
 	signature, err := cose.Sign1(rand.Reader, signer, headers, body, nil)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, `{ "message": "signing failed", "error": "%v" }`, err)
+		sendError(w, "signing failed", err)
 		return
 	}
 	w.Header().Add("Content-Type", "application/json")
@@ -81,12 +119,7 @@ func signHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	listenAddr := ":8080"
-	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
-		listenAddr = ":" + val
-	}
 	http.HandleFunc("/.well-known/did.json", didHandler)
-	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/sign", signHandler)
 
 	contentStatic, err := fs.Sub(static, "static")
@@ -96,6 +129,30 @@ func main() {
 	}
 	http.Handle("/", http.FileServer(http.FS(contentStatic)))
 
-	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
-	log.Fatal(http.ListenAndServe(listenAddr, nil))
+	port := getPort()
+	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", port, port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func sendError(w http.ResponseWriter, message string, err error) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprintf(w, `{ "message": "%s", "error": "%v" }`, message, err)
+}
+
+func getDidHostPort() string {
+	port := getPort()
+	hostname := "localhost%3A" + port
+	if val, ok := os.LookupEnv("WEBSITE_HOSTNAME"); ok {
+		hostname = val
+	}
+	return hostname
+}
+
+func getPort() string {
+	listenAddr := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		listenAddr = ":" + val
+	}
+	return listenAddr
 }
