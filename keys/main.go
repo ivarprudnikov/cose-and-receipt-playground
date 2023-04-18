577c235ae9bc6beb7c1e91da474d09129c22e3e1
ivarprudnikov <ivar.prudnikov@gmail.com>
2023-04-18 19:10:19 +0100

    save generated key in a fileshare to make sure it does not change often

--- keys/main.go
+++ keys/main.go
@@ -4,17 +4,40 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/x509"
+	"os"
 
 	"github.com/veraison/go-cose"
 )
+
+const SHARED_DIR = "/var/tmp/fn/playground-cose-eastus-dir"
 
 var privateKey *ecdsa.PrivateKey
 
 func init() {
 	var err error
-	privateKey, err = ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	if err != nil {
-		panic(err)
+	var keyBytes []byte
+
+	// Load private key from file if it exists
+	filename := SHARED_DIR + "/generated.ecdsa.key"
+	if _, err = os.Stat(filename); err == nil {
+		keyBytes, err = os.ReadFile(filename)
+		if err != nil {
+			privateKey, _ = x509.ParseECPrivateKey(keyBytes)
+		}
+	}
+
+	// create new private key if it doesn't exist
+	if privateKey == nil {
+		privateKey, err = ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		if err != nil {
+			panic(err)
+		}
+		keyBytes, err = x509.MarshalECPrivateKey(privateKey)
+		if err != nil {
+			panic(err)
+		}
+		os.WriteFile(filename, keyBytes, 0600)
 	}
 }
 
