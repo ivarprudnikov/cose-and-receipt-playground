a01fdd36644af58e52db8fa4fc53e04d149f2b2e
ivarprudnikov <ivar.prudnikov@gmail.com>
2024-10-04 22:59:34 +0100

    bootstrap root CA

--- internal/keys/main.go
+++ internal/keys/main.go
@@ -7,25 +7,61 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/hex"
 	"log"
+	"math/big"
 	"os"
+	"time"
 
 	"github.com/veraison/go-cose"
 )
 
 const SHARED_DIR = "/var/tmp/fn/playground-cose-eastus-dir"
 
+var keyFile string = SHARED_DIR + "/generated.ecdsa.key"
+var caFile string = SHARED_DIR + "/ca.der"
+
 var privateKey *ecdsa.PrivateKey
+var rootCert *x509.Certificate
+
+var caTemplate x509.Certificate = x509.Certificate{
+	SerialNumber:          big.NewInt(1),
+	Subject:               pkix.Name{CommonName: "CosePlayground"},
+	NotBefore:             time.Now(),
+	NotAfter:              time.Now().AddDate(10, 0, 0),
+	IsCA:                  true,
+	KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+	BasicConstraintsValid: true,
+}
+
+// var leafTemplate x509.Certificate = x509.Certificate{
+// 	SerialNumber: big.NewInt(2),
+// 	Subject:      pkix.Name{CommonName: "CosePlayground Signer"},
+// 	NotBefore:    time.Now(),
+// 	NotAfter:     time.Now().AddDate(0, 0, 5),
+// 	KeyUsage:     x509.KeyUsageDigitalSignature,
+// 	ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageCodeSigning, x509.ExtKeyUsageMicrosoftCommercialCodeSigning, x509.ExtKeyUsageMicrosoftKernelCodeSigning},
+// }
 
 func init() {
 	var err error
 	var keyBytes []byte
+	var certDer []byte
+	var recreateCa bool = true
+
+	// create shared directory if it doesn't exist
+	if _, err = os.Stat(SHARED_DIR); os.IsNotExist(err) {
+		err = os.MkdirAll(SHARED_DIR, 0700)
+		if err != nil {
+			panic(err)
+		}
+	}
 
 	// Load private key from file if it exists
-	filename := SHARED_DIR + "/generated.ecdsa.key"
-	if _, err = os.Stat(filename); err == nil {
-		keyBytes, err = os.ReadFile(filename)
+	log.Printf("Reading key from file %s", keyFile)
+	if _, err = os.Stat(keyFile); err == nil {
+		keyBytes, err = os.ReadFile(keyFile)
 		if err == nil {
 			privateKey, err = x509.ParseECPrivateKey(keyBytes)
 			if err != nil {
@@ -36,8 +72,19 @@ func init() {
 		}
 	}
 
+	log.Printf("Reading CA from file %s", caFile)
+	if _, err = os.Stat(caFile); err == nil {
+		certDer, err = os.ReadFile(caFile)
+		if err == nil {
+			recreateCa = false
+		} else {
+			log.Printf("Failed to read CA file: %s", err.Error())
+		}
+	}
+
 	// create new private key if it doesn't exist
 	if privateKey == nil {
+		recreateCa = true
 		privateKey, err = ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 		if err != nil {
 			panic(err)
@@ -46,7 +93,28 @@ func init() {
 		if err != nil {
 			panic(err)
 		}
-		os.WriteFile(filename, keyBytes, 0600)
+		log.Printf("Writing key file to %s", keyFile)
+		err = os.WriteFile(keyFile, keyBytes, 0600)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if recreateCa || len(certDer) == 0 {
+		certDer, err = x509.CreateCertificate(rand.Reader, &caTemplate, &caTemplate, &privateKey.PublicKey, privateKey)
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("Writing CA file to %s", caFile)
+		err = os.WriteFile(caFile, certDer, 0600)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	rootCert, err = x509.ParseCertificate(certDer)
+	if err != nil {
+		panic(err)
 	}
 }
 
