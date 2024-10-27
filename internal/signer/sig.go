99246db71d18355fbdc8ea3e35ccc313ed3866d9
ivarprudnikov <ivar.prudnikov@gmail.com>
2024-10-27 21:44:18 +0000

    check the issuer type before signature verification and return error if not did:web

--- internal/signer/sig.go
+++ internal/signer/sig.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/ivarprudnikov/cose-and-receipt-playground/internal/keys"
 	"github.com/veraison/go-cose"
@@ -36,15 +37,23 @@ func VerifySignature(signature []byte, didHttpClient *http.Client) error {
 		return fmt.Errorf("failed to unmarshal signature bytes: %w", err)
 	}
 
+	// transitionary period, get issuer from one of the headers
+	// must be the same in both
 	issuerRaw := msg.Headers.Protected[ISSUER_HEADER_KEY]
 	issuer, ok := issuerRaw.(string)
 	if !ok {
-		return fmt.Errorf("issuer is not a string: %v", issuerRaw)
-	}
-	kidRaw := msg.Headers.Protected[cose.HeaderLabelKeyID]
-	kid, ok := kidRaw.([]byte)
-	if !ok {
-		return fmt.Errorf("kid is not a byte array: %v", kidRaw)
+		// test to see if it is defined elsewhere
+		// protected[CWT_CLAIMS_HEADER].(map[any]any)[CWT_CLAIMS_ISSUER_KEY]
+		cwtRaw := msg.Headers.Protected[CWT_CLAIMS_HEADER]
+		cwt, ok := cwtRaw.(map[any]any)
+		if !ok {
+			return fmt.Errorf("issuer is not present: %v", issuerRaw)
+		}
+		issuerRaw = cwt[CWT_CLAIMS_ISSUER_KEY]
+		issuer, ok = issuerRaw.(string)
+		if !ok {
+			return fmt.Errorf("issuer is not a string: %v", issuerRaw)
+		}
 	}
 	algRaw := msg.Headers.Protected[cose.HeaderLabelAlgorithm]
 	alg, ok := algRaw.(cose.Algorithm)
@@ -52,17 +61,26 @@ func VerifySignature(signature []byte, didHttpClient *http.Client) error {
 		return fmt.Errorf("unexpected alg value: %v", algRaw)
 	}
 
-	log.Printf("resolving issuer did: %s, kid %s, alg %v \n", issuer, kid, alg)
-	didResolver := keys.Did{Issuer: issuer, KeyId: string(kid), Client: didHttpClient}
-	pubKey, err := didResolver.ResolvePublicKey()
-	if err != nil {
-		return fmt.Errorf("failed to resolve public key: %w", err)
+	if strings.HasPrefix(issuer, DidWeb.String()) {
+		kidRaw := msg.Headers.Protected[cose.HeaderLabelKeyID]
+		kid, ok := kidRaw.([]byte)
+		if !ok {
+			return fmt.Errorf("kid is not a byte array: %v", kidRaw)
+		}
+		log.Printf("resolving issuer did: %s, kid %s, alg %v \n", issuer, kid, alg)
+		didResolver := keys.Did{Issuer: issuer, KeyId: string(kid), Client: didHttpClient}
+		pubKey, err := didResolver.ResolvePublicKey()
+		if err != nil {
+			return fmt.Errorf("failed to resolve public key: %w", err)
+		}
+
+		verifier, err := cose.NewVerifier(alg, pubKey)
+		if err != nil {
+			return fmt.Errorf("failed to create signature verifier: %w", err)
+		}
+
+		return msg.Verify(nil, verifier)
 	}
 
-	verifier, err := cose.NewVerifier(alg, pubKey)
-	if err != nil {
-		return fmt.Errorf("failed to create signature verifier: %w", err)
-	}
-
-	return msg.Verify(nil, verifier)
+	return fmt.Errorf("unsupported issuer: %s", issuer)
 }
